36c7ad3ac170126ee0f7610bcc6728d9fe99de9c
TomR809 <littlemanbigwalls@gmail.com>
2025-01-06 13:02:53 +0000

    Add files via upload

---
+++ Influence_Score/Node.cs
@@ -0,0 +1,19 @@
+﻿internal class Node
+{
+    public char name { get;}
+    public int g { get; set; } = int.MaxValue;
+    public int f { get; set; } = int.MaxValue;
+    public Node? parent { get; set; } = null; 
+
+    public Node(char n)
+    {
+        name = n;
+    }
+
+    public void Reset()
+    {
+        g = int.MaxValue; // Set g to "infinity" (initial state)
+        f = int.MaxValue; // Set f to "infinity" (initial state)
+        parent = null;    // Clear parent
+    }
+}
